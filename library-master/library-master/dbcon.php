11746c52b0b7a650c33db0edd765315089368a5a
Ahmed1607 <105608222+Ahmed1607@users.noreply.github.com>
2024-05-08 21:38:32 +0300

    Delete library-master/library-master directory

--- library-master/library-master/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "mis";
-$conn = new mysqli($server, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die();
-}
--- library-master/library-master/member/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mis";
-$conn = new mysqli($server, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die();
-}
